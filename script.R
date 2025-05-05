17b40ebbe876bd6f22bef228798864778fbc7b57
Rob <rob.hyndman@monash.edu>
2025-05-05 18:56:33 +1000

    Further refactor

--- script.R
+++ script.R
@@ -1,9 +1,9 @@
 square <- function(x) {
-  res22 <- x + x
-  return(res22) 
+  res23 <- x + x
+  return(res23) 
 }
 
 cube <- function(x) {
-  res22 <- x * x * x
-  return(res22)
+  res23 <- x * x * x
+  return(res23)
 }
