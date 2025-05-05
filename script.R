84350d3df07786dd839de54531bf4864fe4cea2f
Di <dicook@monash.edu>
2025-05-05 18:56:35 +1000

    Further refactor

--- script.R
+++ script.R
@@ -1,9 +1,9 @@
 square <- function(x) {
-  res69 <- x + x
-  return(res69) 
+  res70 <- x + x
+  return(res70) 
 }
 
 cube <- function(x) {
-  res69 <- x * x * x
-  return(res69)
+  res70 <- x * x * x
+  return(res70)
 }
