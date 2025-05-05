8c99aeb16ccc2b0ef5e8ecd0ba05cee152843fe8
Michael <michael.lydeamore@monash.edu>
2025-05-05 18:56:33 +1000

    Further refactor

--- script.R
+++ script.R
@@ -1,9 +1,9 @@
 square <- function(x) {
-  res14 <- x + x
-  return(res14) 
+  res15 <- x + x
+  return(res15) 
 }
 
 cube <- function(x) {
-  res14 <- x * x * x
-  return(res14)
+  res15 <- x * x * x
+  return(res15)
 }
